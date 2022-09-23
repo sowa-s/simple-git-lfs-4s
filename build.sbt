6ca97fce0f60c294f8807c6274d5861ec638df1e
sowashuhei <sowashuhei@gmail.com>
2022-09-24 02:05:48 +0900

    直接結果をLambdaにPOSTするよう修正

--- build.sbt
+++ build.sbt
@@ -2,21 +2,28 @@ ThisBuild / version := "0.1.0-SNAPSHOT"
 
 ThisBuild / scalaVersion := "2.13.9"
 
+val http4sVersion = "0.23.16"
 val awsSdkVersion = "2.17.279"
 val catsEffectVersion = "3.3.14"
 val circeVersion = "0.14.2"
 val scalaTestVersion = "3.2.13"
+val enumeratumVersion = "1.7.0"
 
 lazy val root = (project in file("."))
   .settings(
     name := "simple-git-lfs-4s",
     assembly / mainClass := Some("simple.git.lfs4s.Main"),
     libraryDependencies ++= Seq(
+      "org.http4s" %% "http4s-dsl" % http4sVersion,
+      "org.http4s" %% "http4s-ember-client" % http4sVersion,
+      "org.http4s" %% "http4s-circe" % http4sVersion,
       "org.typelevel" %% "cats-effect" % catsEffectVersion,
       "software.amazon.awssdk" % "s3" % awsSdkVersion,
       "io.circe" %% "circe-core" % circeVersion,
       "io.circe" %% "circe-generic" % circeVersion,
       "io.circe" %% "circe-parser" % circeVersion,
+      "io.circe" %% "circe-generic-extras" % circeVersion,
+      "com.beachape" %% "enumeratum-circe" % enumeratumVersion,
       "org.scalatest" %% "scalatest" % scalaTestVersion % "test",
       "org.scalatest" %% "scalatest-funsuite" % scalaTestVersion % "test"
     ),
--- src/main/scala/simple/git/lfs4s/Main.scala
+++ src/main/scala/simple/git/lfs4s/Main.scala
@@ -1,14 +1,20 @@
 package simple.git.lfs4s
 
-import cats.data.EitherT
 import cats.effect._
-import cats.effect.std.Console
-import io.circe.{DecodingFailure, Json}
+import io.circe.Json
+import io.circe.generic.auto._
 import io.circe.parser.parse
 import io.circe.syntax._
-import io.circe.generic.auto._
+import org.http4s.circe.CirceEntityCodec._
+import org.http4s.circe._
+import org.http4s.ember.client.EmberClientBuilder
+import org.http4s.{Method, Uri}
 
 object Main extends IOApp {
+
+  val AWS_LAMBDA_RUNTIME_API = sys.env.get("AWS_LAMBDA_RUNTIME_API")
+  val INVOCATION_ID = sys.env.get("INVOCATION_ID")
+  val S3_BUCKET_NAME = sys.env.get("S3_BUCKET_NAME")
 
   override def run(args: List[String]): IO[ExitCode] = {
     (for {
@@ -17,16 +23,24 @@ object Main extends IOApp {
         Json.obj(("error", Json.fromString("empty body")))
       )
     } yield for {
-      response <- Console[IO]
-        .print(
-          Response.success(body).asJson
-        )
+      response <- postLambdaResponse(body = args.mkString("").asJson)
         .as(ExitCode.Success)
     } yield response)
       .getOrElse(
-        Console[IO]
-          .print(Response.success(Json.fromString(args.mkString(""))).asJson)
-          .as(ExitCode.Success)
+        postLambdaResponse(body = args.mkString("").asJson)
       )
+  }
+  def postLambdaResponse(body: Json): IO[Unit] = {
+    val request = org.http4s.Request
+      .apply[IO](
+        Method.POST,
+        Uri.unsafeFromString(
+          s"http://${AWS_LAMBDA_RUNTIME_API}/2018-06-01/runtime/invocation/$INVOCATION_ID/response"
+        )
+      )
+      .withEntity(Response.success(body).asJson)
+    EmberClientBuilder.default[IO].build.use { client =>
+      client.expect[Unit](request)
+    }
   }
 }
